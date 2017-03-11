47533c980bcddf75afa1e76a600a59c046af5f92
Kamil Jarząb <kja@kiwu.pl>
2017-03-11 13:02:48 +0100

    Added background

--- js/game.js
+++ js/game.js
@@ -1,16 +1,32 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 
-game.state.add('play', {
+game.state.add("play", {
     preload: function() {
-        game.load.image('skeleton', 'assets/enemies/skeleton1.png');
+        game.load.image("skeleton", "assets/enemies/skeleton1.png");
+        this.game.load.image("forest-back", "assets/background/forest/parallax-forest-back-trees.png");
+        this.game.load.image("forest-lights", "assets/background/forest/parallax-forest-lights.png");
+        this.game.load.image("forest-middle", "assets/background/forest/parallax-forest-middle-trees.png");
+        this.game.load.image("forest-front", "assets/background/forest/parallax-forest-front-trees.png");
     },
     create: function() {
-        var skeletonSprite = game.add.sprite(450, 290, 'skeleton');
+        var state = this;
+ 
+        this.background = this.game.add.group();
+        // setup each of our background layers to take the full screen
+        ['forest-back', 'forest-lights', 'forest-middle', 'forest-front']
+            .forEach(function(image) {
+                var bg = state.game.add.tileSprite(0, 0, state.game.world.width,
+                    state.game.world.height, image, '', state.background);
+                bg.tileScale.setTo(4,4);
+            });
+        
+        var skeletonSprite = game.add.sprite(450, 290, "skeleton");
         skeletonSprite.anchor.setTo(0.5, 0.5);
+        
     },
     render: function() {
-        game.debug.text('Adventure Awaits!', 250, 290);
+        game.debug.text("Adventure Awaits!", 250, 290);
     }
 });
 
-game.state.start('play');
+game.state.start("play");
