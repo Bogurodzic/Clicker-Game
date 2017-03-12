399b9edf31aff3010c55da8c7153041b77188ea5
Kamil Jarząb <kja@kiwu.pl>
2017-03-12 13:25:41 +0100

    Monsters now have hp. Added on click animation

--- js/game.js
+++ js/game.js
@@ -13,6 +13,12 @@ game.state.add("play", {
     },
     create: function() {
         var state = this;
+        
+        this.player = {
+            clickDmg: 1,
+            gold: 0
+        };
+        
         this.background = this.game.add.group();
         // setup each of our background layers to take the full screen
         ['forest-back', 'forest-lights', 'forest-middle', 'forest-front']
@@ -24,10 +30,10 @@ game.state.add("play", {
         
         
         var monsterData = [
-            {name: "Skeleton", image: "skeleton"},
-            {name: "Nagaruda", image: "nagaruda"},
-            {name: "Arcane Drake", image: "arcanedrake"},
-            {name: "Deceleon", image: "deceleon"}
+            {name: "Skeleton", image: "skeleton", maxHealth: 5},
+            {name: "Nagaruda", image: "nagaruda", maxHealth: 10},
+            {name: "Arcane Drake", image: "arcanedrake", maxHealth: 15},
+            {name: "Deceleon", image: "deceleon", maxHealth: 20}
         ]
         
         this.monsters = this.game.add.group();
@@ -40,27 +46,95 @@ game.state.add("play", {
             monster.anchor.setTo(0.5);
             //reference to the database
             monster.details = data;
+            monster.health = monster.maxHealth = data.maxHealth;
 
             
             //enable input so we can click it!
             monster.inputEnabled = true;
             monster.events.onInputDown.add(onClickMonster, state)
             
+            // hook into health and lifecycle events
+            monster.events.onKilled.add(onKilledMonster, state);
+            monster.events.onRevived.add(onRevivedMonster, state);
+            
         });
         
         this.currentMonster = this.monsters.getRandom();
         this.currentMonster.position.set(this.game.world.centerX + 100, this.game.world.centerY);
+        
+        this.monsterInfoUI = this.game.add.group();
+        this.monsterInfoUI.position.setTo(this.currentMonster.x - 250, this.currentMonster.y);
+        this.monsterNameText = this.monsterInfoUI.addChild(this.game.add.text(0, 0, this.currentMonster.details.name), {
+            font: "48px Arial Black",
+            fill: "#fff",
+            strokeThickness: 4
+        });
+        this.monsterHealthText = this.monsterInfoUI.addChild(this.game.add.text(0, 30, this.currentMonster.health + " HP"), {
+            font: "32px Arial Black",
+            fill: "#ff0000",
+            strokeThickness: 4
+        });
+        
+        this.dmgTextPool = this.add.group();
+        var dmgText;
+              
+        for (var i=0; i<50; i++){
+            dmgText = this.add.text(0, 0, "1", {
+                font: "64px Arial Black",
+                color: "#fff",
+                strokeThickness: 4
+            });
+            //start out not existing, so we don't draw it yet
+            dmgText.exists = false;
+            dmgText.tween = game.add.tween(dmgText).to({
+                alpha: 0,
+                y: 100,
+                x: this.game.rnd.integerInRange(100, 700)
+            }, 1000, Phaser.Easing.Cubic.Out);
+            
+            dmgText.tween.onComplete.add(function(text, tween){
+               text.kill(); 
+            });
+            this.dmgTextPool.add(dmgText);
+        }
+        
 
         function onClickMonster(){
-            this.currentMonster.position.set(1000, this.game.world.centerY);
+            this.currentMonster.damage(this.player.clickDmg);
+            this.monsterHealthText.text = this.currentMonster.alive ? this.currentMonster.health + " HP" : "DEAD";
+            console.log(this.currentMonster.health);
             
+            var dmgText = this.dmgTextPool.getFirstExists(false);
+            if (dmgText) {
+                dmgText.text = this.player.clickDmg;
+                dmgText.reset(game.input.mousePointer.x, game.input.mousePointer.y);
+                dmgText.alpha = 1;
+                dmgText.tween.start();
+            }
+        };
+        
+        function onKilledMonster(monster){
+            //move the monster off-screen once again
+            monster.position.set(1000, this.game.world.centerY);
+            
+            //pick a new monster
             this.currentMonster = this.monsters.getRandom();
+            //make sure they are fully healed
+            this.currentMonster.revive(this.currentMonster.maxHealth);
+        };
+        
+        function onRevivedMonster(monster){
+            this.monsterNameText.text = this.currentMonster.details.name;
+            this.monsterHealthText.text = this.currentMonster.health + " HP";
             this.currentMonster.position.set(this.game.world.centerX + 100, this.game.world.centerY);
         }
+        
 
     },
+    update: function() {
+    },
     render: function() {
-        game.debug.text(this.currentMonster.details.name, this.game.world.centerX - this.currentMonster.width / 2, this.game.world.centerY);
+        //game.debug.text(this.currentMonster.details.name, this.game.world.centerX - this.currentMonster.width / 2, this.game.world.centerY);
     }
 });
 
