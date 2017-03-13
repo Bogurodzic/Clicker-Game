bd430f6066f6d28ebc45b59f59e58067a0fd84c6
Kamil Jarząb <kja@kiwu.pl>
2017-03-13 15:56:51 +0100

    Added skills. Added damage per second

--- js/game.js
+++ js/game.js
@@ -11,14 +11,39 @@ game.state.add("play", {
         this.game.load.image("forest-middle", "assets/background/forest/parallax-forest-middle-trees.png");
         this.game.load.image("forest-front", "assets/background/forest/parallax-forest-front-trees.png");
         this.game.load.image('gold_coin', 'assets/icons/I_GoldCoin.png');
+        this.game.load.image("dagger", "assets/icons/W_Dagger002.png");
+        this.game.load.image("sword1", "assets/icons/S_Sword01.png");
+        //build panel for upgrade
+        
+
+        // build panel for upgrades
+        var bmd = this.game.add.bitmapData(250, 500);
+        bmd.ctx.fillStyle = "#9a783d";
+        bmd.ctx.strokeStyle = "#35371c";
+        bmd.ctx.lineWidth = 12;
+        bmd.ctx.fillRect(0, 0, 250, 500);
+        bmd.ctx.strokeRect(0, 0, 250, 500);
+        this.game.cache.addBitmapData("upgradePanel", bmd);
+
+        var buttonImage = this.game.add.bitmapData(476, 48);
+        buttonImage.ctx.fillStyle = "#e6dec7";
+        buttonImage.ctx.strokeStyle = "#35371c";
+        buttonImage.ctx.lineWidth = 4;
+        buttonImage.ctx.fillRect(0, 0, 225, 48);
+        buttonImage.ctx.strokeRect(0, 0, 225, 48);
+        this.game.cache.addBitmapData("button", buttonImage);
+        
+
     },
     create: function() {
-        var state = this;
+        var state = this; 
         
         this.player = {
             clickDmg: 1,
-            gold: 0
+            gold: 10,
+            dps: 0
         };
+        
         
         this.background = this.game.add.group();
         // setup each of our background layers to take the full screen
@@ -29,6 +54,33 @@ game.state.add("play", {
                 bg.tileScale.setTo(4,4);
             });
         
+        this.upgradePanel = this.game.add.image(10, 70, this.game.cache.getBitmapData("upgradePanel"));
+        var upgradeButtons = this.upgradePanel.addChild(this.game.add.group());
+        upgradeButtons.position.setTo(8, 8);  
+        
+
+        
+       
+        
+        var upgradeButtonsData = [
+            {icon: "dagger", name: "Attack", level: 1, cost: 5, purchaseHandler: function(button, player){
+                state.player.clickDmg += 1;
+            }},
+            {icon: "sword1", name: "Auto-Attack", level: 0, cost: 10, purchaseHandler: function(button, player){
+               state.player.dps += 5; 
+            }}
+        ];
+ 
+        var button;       
+        upgradeButtonsData.forEach(function(data, index){
+            button = this.game.add.button(0, (50*index), this.game.cache.getBitmapData("button"));
+            button.icon = button.addChild(this.game.add.image(6, 6, data.icon));
+            button.text = button.addChild(this.game.add.text(42, 6, data.name, {font: "16px Arial Black"}));
+            button.details = data;
+            button.costText = button.addChild(this.game.add.text(42, 24, "Cost: " + data.cost, {font: "16px Arial Black"}));
+            button.events.onInputDown.add(onUpgradeButtonClick, this);
+            upgradeButtons.addChild(button);
+        });
         
         var monsterData = [
             {name: "Skeleton", image: "skeleton", maxHealth: 5},
@@ -61,10 +113,10 @@ game.state.add("play", {
         });
         
         this.currentMonster = this.monsters.getRandom();
-        this.currentMonster.position.set(this.game.world.centerX + 100, this.game.world.centerY);
+        this.currentMonster.position.set(this.game.world.centerX + 120, this.game.world.centerY);
         
         this.monsterInfoUI = this.game.add.group();
-        this.monsterInfoUI.position.setTo(this.currentMonster.x - 250, this.currentMonster.y);
+        this.monsterInfoUI.position.setTo(this.currentMonster.x - 225, this.currentMonster.y);
         this.monsterNameText = this.monsterInfoUI.addChild(this.game.add.text(0, 0, this.currentMonster.details.name), {
             font: "48px Arial Black",
             fill: "#fff",
@@ -110,6 +162,8 @@ game.state.add("play", {
         this.coins.setAll("inputEnabled", true);
         this.coins.setAll("goldValue", 1);
         this.coins.callAll("events.onInputDown.add", "events.onInputDown", onClickCoin, this);
+        
+        this.dpsTimer = this.game.time.events.loop(100, onDps, this);
         
         function onClickMonster(){
             this.currentMonster.damage(this.player.clickDmg);
@@ -160,6 +214,29 @@ game.state.add("play", {
             //remove the coin
             coin.kill();
         };
+        
+        function onUpgradeButtonClick(button, pointer){
+            console.log(state.player);
+
+            if (state.player.gold - button.details.cost >= 0){
+                state.player.gold -= button.details.cost;
+                state.playerGoldText.text = "Gold: " + state.player.gold;
+                button.details.level++;
+                button.text.text = button.details.name + ": " + button.details.level;
+                button.details.purchaseHandler.call(this, button, this.player);
+            }
+        };
+        
+        function onDps(){
+            if (this.player.dps > 0){
+                if (this.currentMonster && this.currentMonster.alive){
+                    var dmg = this.player.dps/10;
+                    this.currentMonster.damage(dmg);
+                    //update the health text
+                    this.monsterHealthText.text = this.currentMonster.alive ? Math.round(this.currentMonster.health) + " HP" : "DEAD";
+                }
+            }
+        }
         
 
     },
