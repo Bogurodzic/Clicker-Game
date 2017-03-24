e3b82bbd4afb56a05647904278c2909e714bac9a
Kamil Jarząb <kja@kiwu.pl>
2017-03-24 17:32:44 +0100

    Added loop which creates menu buttons

--- js/menu.js
+++ js/menu.js
@@ -2,15 +2,38 @@ var menuState = {
     create: function() {
         var self = this;
         
-        this.startButton = this.game.add.image(game.world.centerX, 175, "button");
-        this.startButton.anchor.setTo(0.5);
-        this.startButton.inputEnabled = true;
-        this.startButton.events.onInputDown.add(startGame, self);
-        this.startButtonText = this.startButton.addChild(this.game.add.text(0, 0, "Play"));
-        this.startButtonText.anchor.setTo(0.5);
+        this.buttonsList = [
+            {text: "Play", press: startGame},
+            {text: "Options", press: gameOptions},
+            {text: "Credits", press: gameCredits}
+        ];
         
+        this.menuButtons = this.game.add.group();
+        this.menuButtons.x = game.world.centerX;
+        this.menuButtons.y = game.world.centerY-90;
+        
+        var menuButton;
+        this.buttonsList.forEach(function(data, index){
+            menuButton = this.game.add.image(0, 60 * index, "button");
+            menuButton.anchor.setTo(0.5);
+            menuButton.inputEnabled = true;
+            menuButton.events.onInputDown.add(data.press);
+            menuButton.menuButtonText = menuButton.addChild(this.game.add.text(0, 0, data.text));
+            menuButton.menuButtonText.anchor.setTo(0.5);
+            self.menuButtons.addChild(menuButton);
+        });
+              
         function startGame() {
             game.state.start("play");
-        }
+        };
+        
+        function gameOptions() {
+            
+        };
+        
+        function gameCredits() {
+            
+        };
+        
     }
 }
